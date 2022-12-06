2532d1030df98ed1778a47f7e9e6dccb2117798c
1145308 <sangeetakurmi@gmail.com>
2022-12-06 14:07:04 +0530

    add some changes

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mark-12-funWithTriangles
  Here we are the exploring much on Triangles !
+
+
+# Traingles :
+
+A web app which helps user to learn about triangles. In this app you can play quiz, find hypotenuse of a triangle, area of a triangle and check whether the given angles form a triangle or not . This app is made by using HTML ,CSS and VanillaJS . 
+
+# How to use this web app :
+
+a. Is Triangle: Put the angles in input fields and check the angles form a triangle or not.
+
+b. Quiz: Play the quiz and know how much you know about triangles.
+
+c. Hypotenuse: Check the hypotenuse of a trinagle by entering the base and height.
+
+d. Area: Put the side values and check the area of the traingle.
--- index.js
+++ index.js
@@ -8,20 +8,27 @@ function calculateSumOfAngles(angle1 , angle2 , angle3){
     const sumOfAngles=angle1 + angle2 + angle3 ;
     return sumOfAngles;
 
-}
+
+}   
 
 function isTrinagle(){
+
     const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
-    if(sumOfAngles === 180 )
+    if(sumOfAngles > 0)
+      if(sumOfAngles === 180 )
      {
         outputBtn.innerText="Yay ! The Angles will form a Triangle 🥳";
        }     
        
     else{
         outputBtn.innerText="Naah ! The Angles will not form a Triangle 😢";
+    } 
+
+    else{
+        outputBtn.innerText="Invalid values ! Angles can not be less than or equal to 0 ";
     }
     }
 
 
 checkBtn.addEventListener("click" , isTrinagle)
-//    console.log("hu");
+
